bd07713feff69e09f170bc8f9d9d8a173b9d15b0
Eduard Beutel <eduard.beutel@yahoo.com>
2015-11-02 19:04:44 +0100

    Basic example cleanup

---
+++ examples/basic/results/Person.java
@@ -0,0 +1,16 @@
+public class Person
+{
+    private int age;
+    private String name;
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+}
